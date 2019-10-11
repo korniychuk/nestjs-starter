deef6b740040c755bb866c13805e8390a974de2f
Anton Korniychuk <dev@korniychuk.pro>
2019-10-11 09:47:12 +0300

    update merge tool

--- tools/merge-with-repository-template.sh
+++ tools/merge-with-repository-template.sh
@@ -6,6 +6,8 @@ declare -r colorYellow='\033[0;33m'
 declare -r colorBYellow='\033[1;33m'
 declare -r colorNc='\033[0m' # No Color
 
+declare -r tplRepoGit='git@github.com:korniychuk/wallaby-ts-starter.git'
+
 function execute() {
   exec "${@}" 2> >(awk "${prefixAwkPattern}" 1>&2) | awk "${prefixAwkPattern}"
 }
@@ -13,7 +15,7 @@ function execute() {
 execute echo '------------------------ Merge With Template :: BEGIN ------------------------'
 
 if [[ "$(git remote -v 2> /dev/null | grep -E '^template')" == "" ]]; then
-    execute git remote add template git@github.com:korniychuk/wallaby-ts-starter.git
+    execute git remote add template "${tplRepoGit}"
     execute echo "'template' origin added"
 fi
 
@@ -22,12 +24,11 @@ execute git fetch template
 if [[ "${1}" == "merge" ]]; then
   execute git merge --allow-unrelated-histories template/master --no-edit
 elif [[ "${1}" == "check" ]]; then
-#  execute git merge --allow-unrelated-histories template/master --no-edit
   if [[ -z "$(git log HEAD..template/master --oneline)" ]]; then
     execute echo -e "${colorGreen}The template repository has not changed${colorNc}"
   else
     execute echo -e "${colorYellow}Warning! The template repository contains updates." \
-                    "Execute '${colorBYellow}npm run tpl-check:merge${colorYellow}' to apply the updates.${colorNc}" >&2
+                    "Execute '${colorBYellow}npm run tpl-repo:merge${colorYellow}' to apply the updates.${colorNc}" >&2
   fi
 fi
 
