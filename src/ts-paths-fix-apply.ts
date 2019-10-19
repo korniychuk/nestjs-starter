ff4608433a36732a217baaa688da807fe39a00d1
Anton Korniychuk <dev@korniychuk.pro>
2019-10-19 05:23:45 +0300

    remov unneeded env variable

--- src/ts-paths-fix-apply.ts
+++ src/ts-paths-fix-apply.ts
@@ -4,8 +4,6 @@ import * as moduleAliases from 'module-alias';
 import { getTsConfigPaths } from './ts-paths-fix-helpers';
 
 export const applyTsPathFix = (): void => {
-  if (process.env.IS_TS_NODE) return;
-
   getTsConfigPaths().forEach((one) => {
     const absoluteTarget = path.join(__dirname, one.path);
 
