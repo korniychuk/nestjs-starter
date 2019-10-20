9d95f8e9aa1cc34d37314d3ddc8e4104171bf703
Anton Korniychuk <dev@korniychuk.pro>
2019-10-20 03:42:09 +0300

    types fixed

--- src/ts-paths-fix-apply.ts
+++ src/ts-paths-fix-apply.ts
@@ -1,10 +1,24 @@
 import * as path from 'path';
 import * as moduleAliases from 'module-alias';
 
-import { getTsConfigPaths } from './ts-paths-fix-helpers';
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const { getTsConfigPaths } = require('./ts-paths-fix-helpers');
+
+interface PathAlias {
+  /**
+   * @example: `@app` safe
+   * @example: `@app/*` unsafe
+   */
+  alias: string;
+  /**
+   * @example: `app` safe
+   * @example: `./app/*` unsafe
+   */
+  path: string;
+}
 
 export const applyTsPathFix = (): void => {
-  getTsConfigPaths().forEach((one) => {
+  getTsConfigPaths().forEach((one: PathAlias) => {
     const absoluteTarget = path.join(__dirname, one.path);
 
     moduleAliases.addAlias(one.alias, absoluteTarget);
