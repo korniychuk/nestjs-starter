995f722450681939ee74f07e9f4804e2311d3fad
Anton Korniychuk <dev@korniychuk.pro>
2019-12-15 02:11:04 +0200

    .eslintrc.js merged

--- .eslintrc.js
+++ .eslintrc.js
@@ -76,5 +76,69 @@ module.exports = {
     'no-mixed-operators': 'off',
     'strict': 'off',
     'template-curly-spacing': ['error', 'always'],
+    'semi-style': 'off',
+    'no-multi-spaces': 'off'
   },
+  overrides: [
+    {
+      files: ['*.ts'],
+      parser: '@typescript-eslint/parser',
+      parserOptions: {
+        project: './tsconfig.json',
+        ecmaVersion: 2020,
+      },
+      "settings": {
+        "import/parsers":  {
+          "@typescript-eslint/parser": [".ts"]
+        },
+        "import/resolver": {
+          "typescript": {
+            // use <root>/path/to/folder/tsconfig.json
+            // "directory": "./path/to/folder"
+          },
+        },
+      },
+      plugins: [
+        '@typescript-eslint',
+      ],
+      extends: [
+        'plugin:import/typescript',
+        'plugin:@typescript-eslint/eslint-recommended',
+        'plugin:@typescript-eslint/recommended',
+        'plugin:@typescript-eslint/recommended-requiring-type-checking',
+      ],
+      rules: {
+        '@typescript-eslint/restrict-plus-operands': 'error',
+        'import/no-extraneous-dependencies': [
+          'error',
+          { devDependencies: ['**/*spec.ts'] },
+        ],
+        'no-useless-constructor': 'off',
+        '@typescript-eslint/no-useless-constructor': 'error',
+        '@typescript-eslint/no-explicit-any': 'off',
+        '@typescript-eslint/no-non-null-assertion': 'off',
+        '@typescript-eslint/member-delimiter-style': ['error', { // Prevents us from using any delimiter for interface properties.
+          'multiline': {
+            'delimiter': 'semi',
+            'requireLast': true
+          },
+          'singleline': {
+            'delimiter': 'semi',
+            'requireLast': false
+          }
+        }],
+        '@typescript-eslint/indent': 'off', // This is the job of StandardJS, they are competing rules so we turn off the Typescript one.
+        '@typescript-eslint/no-use-before-define': ['error', {
+          functions: false,
+          classes: false,
+        }],
+        "@typescript-eslint/explicit-member-accessibility": ["error"],
+        'function-paren-newline': 'off',
+        'implicit-arrow-linebreak': 'off',
+        '@typescript-eslint/explicit-function-return-type': 'off',
+        '@typescript-eslint/unbound-method': 'off',
+        '@typescript-eslint/no-inferrable-types': 'off',
+      },
+    },
+  ],
 };
